e77e5eabad3b4f2a357a0fcdd740d2e67b18998f
falgune2701 <falgunemondal2@gmail.com>
2025-08-09 07:35:59 +0530

    create blaskConfeeti this animation are show when all tasks are complete

--- CodeAlpha_ToDoList/script.js
+++ CodeAlpha_ToDoList/script.js
@@ -56,7 +56,7 @@ const deleteTask = (index) => {
  saveTasks();
 };
 
-// update status
+// update stats
 const updateStats = () => {
     const completedTasks = tasks.filter((task) => task.completed).length;
     const totalTasks = tasks.length;
@@ -64,6 +64,10 @@ const updateStats = () => {
     const progress = (completedTasks / totalTasks)*100;
     const progressBar = document.getElementById('progress');
     progressBar.style.width = `${progress}%`;
+    
+    if(tasks.length && completedTasks == totalTasks){
+        blaskConfetti();
+    }
 
 };
 
@@ -91,3 +95,45 @@ const updateTasksList = () => {
 
     });
 };
+
+const blaskConfetti = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
